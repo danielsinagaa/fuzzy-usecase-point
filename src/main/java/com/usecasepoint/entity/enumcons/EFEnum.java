24fed277773c9ee5ccb6b38d8686f751a63c7b89
Daniel Ihot Junior Sinaga <nieljrnaga@gmail.com>
2021-07-06 23:28:13 +0700

    changes

--- src/main/java/com/usecasepoint/entity/enumcons/EFEnum.java
+++ src/main/java/com/usecasepoint/entity/enumcons/EFEnum.java
@@ -4,14 +4,14 @@ import lombok.Getter;
 
 @Getter
 public enum EFEnum {
-    E1("E1","","", 1.5),
-    E2("E2","","",0.5),
-    E3("E3", "","",1.0),
-    E4("E4","","",0.5),
-    E5("E5","","",1.0),
-    E6("E6","", "", 2.0),
-    E7("E7", "", "", -1.0),
-    E8("E8","","",-1.0);
+    E1("E1","Familiarity with system development process being used","pemahaman dengan proses pengembangan sistem yang digunakan", 1.5),
+    E2("E2","Application experience","experience yang diperoleh user pada saat menggunakan produk",0.5),
+    E3("E3", "Object-oriented experience ","Penggunaan oop(obejct oriented programming) pada aplikasi",1.0),
+    E4("E4","Lead analyst capability ","Kapabilitas dari analisis sistem pada saat proses pengembangan",0.5),
+    E5("E5","Motivation","Motivasi tim dalam mengembangkan produk",1.0),
+    E6("E6","Requirements stability", "Stabilitas requirement pada saat pengembangan produk", 2.0),
+    E7("E7", "Part time staff", "Pekerja part time yang berada pada tim pengembang produk", -1.0),
+    E8("E8","Difficulty of programming language","Kesulitan bahasa pemograman yang digunakan pada saat mengambangkan produk",-1.0);
 
     EFEnum(String code, String deskripsi, String uraian, Double bobot) {
         this.code = code;
--- src/main/java/com/usecasepoint/entity/enumcons/TCFEnum.java
+++ src/main/java/com/usecasepoint/entity/enumcons/TCFEnum.java
@@ -9,15 +9,15 @@ public enum TCFEnum {
     T2("T2", "Response Time Or Throughput Performance Objective", "Target dan sasaran waktu untuk merespon kinerja dan hasil saat memproses beban kerja yang ditentukan. ", 1.0),
     T3("T3", "End-User Online Efficiency ", "Efisiensi waktu yang digunakan oleh user ketika aktif/online menggunakan produk", 1.0),
     T4("T4", "Complex Internal Processing", "Kompleksitas dari internal pengembang/developer dalam memproses produk", 1.0),
-    T5("T5", "Reusability Of Code", "", 1.0),
-    T6("T6","","",0.5),
-    T7("T7","","", 0.5),
-    T8("T8","","",2.0),
-    T9("T9", "", "", 1.0),
-    T10("T10","","",1.0),
-    T11("T11","","",1.0),
-    T12("T12","","",1.0),
-    T13("T13","","",1.0);
+    T5("T5", "Reusability Of Code", "Penggunaan code yang dapat digunakan kembali dalam pembaharuan versi atau pengembangan software yang baru menggunakan aset yang sudah ada sebelumnya.", 1.0),
+    T6("T6","Easy To Install","Produk data diunduh secara mudah oleh user",0.5),
+    T7("T7","Ease of Use","Produk dapat digunakan dengan mudah dan cepat dipahami oleh user", 0.5),
+    T8("T8","Portability","Kemudahaan yang sama bagi user dalam penggunaan produk dari lingkungan yang berbeda",2.0),
+    T9("T9", "Ease of Change", "Perubahan yang mudah dan cepat mengikuti kebutuhan pasar", 1.0),
+    T10("T10","Concurrency ","Permintaan user yang bisa dilaksanakan dalam sekaligus atau satu waktu",1.0),
+    T11("T11","Special Security Objectives Included","Keamaan aplikasi bagi user ",1.0),
+    T12("T12","Direct Access For Third Parties ","Akses langsung pihak ketiga yang berkerjasama dengan produk",1.0),
+    T13("T13","Special User Training Required","Pelatihan khusus atau petunjuk khusus bagi user dalam menggunakan produk",1.0);
 
     TCFEnum(String code, String deskripsi, String uraian, Double bobot) {
         this.code = code;
